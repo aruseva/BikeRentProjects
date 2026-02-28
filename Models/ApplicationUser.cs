55a25e0d8c5dc607105bd29bdca0ddb2e1351bf6
aruseva <aruseva@abv.bg>
2026-02-28 12:58:10 +0200

    Create Models

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Microsoft.AspNetCore.Identity;
+
+namespace BikeRentProjects.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public virtual ICollection<RentalRequest> RentalRequests { get; set; }
+    }
+}
--- Models/Bike.cs
+++ Models/Bike.cs
@@ -1,7 +1,14 @@
 ﻿namespace BikeRentProjects.Models
 {
-    public class Bike
+    public class Bike:BaseEntity
     {
-        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int  TypeBikeID { get; set; }
+        public virtual TypeOfBike? TypeBike { get; set; }
+        public string Location  { get; set; }
+        public string Description { get; set; }
+        public decimal PriceForHour { get; set; }
+        public string Image { get; set; }
     }
 }
